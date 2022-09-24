9214afe61fd928180a0e1169bb86a654155dbd93
Celil6p <vublayancanavar@gmail.com>
2022-09-24 23:00:23 +0300

    Empty string animation, and place holder text for empty task list added.

--- js.js
+++ js.js
@@ -1,9 +1,23 @@
+//VIBRATION EFFECT FOR INPUT BOX
+
+var element = document.getElementById('entry');
+var removeError = function() { element.classList.remove('error'); };
+var addError = function() { element.classList.add('error'); };
+
+
 document.querySelector('#push').onclick = function(){
+    const placeHD = document.getElementById('placeHolder');
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
+        //alert("Please Enter a Task")
+        addError();
     }
 
+//ENTRY PUSH FOR TODO LIST
+
     else{
+        if(placeHD != null){
+        placeHD.remove();
+        }
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
